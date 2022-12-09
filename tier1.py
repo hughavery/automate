ea90f6e8894f4ea60c28b7c44c0c21447fd12f59
Hugh Avery <hav13@uclive.ac.nz>
2022-12-09 16:15:10 +1300

    Fixed bugs better recommendations

--- tier1.py
+++ tier1.py
@@ -1,12 +1,21 @@
 import csv
 
 
+def bad_tier_choice(column_data):
+    """checks if a column is relevant for model"""
+    if 1 < len(column_data) / len(set(column_data)) < 1.2:
+        return True
+
+    if len(set(column_data)) == 1:
+        return True
+
+    return False
+
 def read_file_and_organise_hierarchy(filename):
     with open(filename, encoding='utf-8-sig') as file:
         reader = list(csv.reader(file))
         number_of_columns = len(reader[0])
         number_of_rows = len(reader)
-        
         dic = {}
         col_names = reader[0]
         candidates_tier1 = []
@@ -15,9 +24,7 @@ def read_file_and_organise_hierarchy(filename):
         for i in range(len(reader[0])):
             dic[i] = []
             
-    
         #Map column number to a list of all corresponding values in CSV
-      
         for i in range(1,number_of_rows):
             for j in range(number_of_columns):
                 dic[j].append(reader[i][j])
@@ -27,22 +34,25 @@ def read_file_and_organise_hierarchy(filename):
     dic = dict(sorted(dic.items(), key=lambda item: len(set(item[1])),reverse=True))
 
     for i in dic:
+        column_data = dic[i]
+        if bad_tier_choice(column_data):
+            continue
+        
         #check to see if col is numeric 
-        if dic[i][0].isnumeric() == False:
+        if column_data[0].isnumeric() == False:
             #checks to see if all values in a col are unique
-            if len(dic[i]) != len(set(dic[i])):
-                candidates_n_tier.append(col_names[i])
-            #checks to see if all values in a col are unique
-            else:
+            if len(column_data) == len(set(column_data)):
+                #tier1
                 candidates_tier1.append(col_names[i])
+            else:
+                #other tiers
+                candidates_n_tier.append(col_names[i])
+
     return candidates_tier1,candidates_n_tier
 
-
 def main():
-    filename = "covid-19 20-April.csv"
-
+    filename = "ecan_data-17-sep-19.csv"
     T1,other_tiers = read_file_and_organise_hierarchy(filename)
-    print(other_tiers)
     Max_number_of_levels = len(other_tiers) + 1
     number_of_levels = int(input(f"Please specify how many levels you would like in your application(Max num layers --> {Max_number_of_levels}) "))
     while number_of_levels < 1 or number_of_levels > Max_number_of_levels:
@@ -51,11 +61,10 @@ def main():
 
 
     print(f"Available tier 1 subjects --->  {T1}")
-    
+
     for i in range(number_of_levels-1):
         print(f"Recomended tier {i+2} subject --->  {other_tiers[i]}")
-        # print(f"Recomended tier 2 subject --->  {other_tiers[1]}")
-
+        
 
 main()
 
