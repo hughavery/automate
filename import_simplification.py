7aff199c13cad7d78cf58dc0ce309c8a752c285e
Hugh Avery <hav13@uclive.ac.nz>
2022-12-12 14:57:45 +1300

    can now detect dates. suitable for multi-tier

--- import_simplification.py
+++ import_simplification.py
@@ -1,15 +1,27 @@
 import csv
+from dateutil import parser
 
 
 def bad_tier_choice(column_data):
     """checks if a column is relevant for model"""
-    if 1 < len(column_data) / len(set(column_data)) < 1.2:
+    ratio = len(column_data) / len(set(column_data))
+    if 1 < ratio < 1.2:
         return True
 
     if len(set(column_data)) == 1:
         return True
 
     return False
+
+
+
+def is_date(string):
+  try:
+    date = parser.parse(string)
+    return True
+  except ValueError:
+    return False
+
 
 def read_file_and_organise_hierarchy(filename):
     with open(filename, encoding='utf-8-sig') as file:
@@ -35,35 +47,39 @@ def read_file_and_organise_hierarchy(filename):
 
     for i in dic:
         column_data = dic[i]
-        if bad_tier_choice(column_data):
+        if bad_tier_choice(column_data) or is_date(column_data[0]) or column_data[0].isnumeric():
             continue
-        
-        #check to see if col is numeric 
-        if column_data[0].isnumeric() == False:
-            #checks to see if all values in a col are unique
-            if len(column_data) == len(set(column_data)):
-                #tier1
-                candidates_tier1.append(col_names[i])
-            else:
-                #other tiers
-                candidates_n_tier.append(col_names[i])
+
+        if len(column_data) == len(set(column_data)):
+            #tier1
+            candidates_tier1.append(col_names[i])
+        else:
+            #other tiers
+            candidates_n_tier.append(col_names[i])
 
     return candidates_tier1,candidates_n_tier
 
 def main():
+    # filename = "covid-19 16-April.csv"
+    # filename = "covid-19 20-April.csv"
     filename = "ecan_data-17-sep-19.csv"
-    T1,other_tiers = read_file_and_organise_hierarchy(filename)
+
+    t1,other_tiers = read_file_and_organise_hierarchy(filename)
+    if len(t1) == 0:
+        print("No unique columns in csv try with another set of data")
+        return None
     Max_number_of_levels = len(other_tiers) + 1
     number_of_levels = int(input(f"Please specify how many levels you would like in your application(Max num layers --> {Max_number_of_levels}) "))
     while number_of_levels < 1 or number_of_levels > Max_number_of_levels:
         print("Your chosen hierarchy is not valid\ntry again")
         number_of_levels = int(input(f"Please specify how many levels you would like in your application(Max num layers --> {Max_number_of_levels}) "))
 
-
-    print(f"Available tier 1 subjects --->  {T1}")
-
-    for i in range(number_of_levels-1):
-        print(f"Recomended tier {i+2} subject --->  {other_tiers[i]}")
+    other_tiers.reverse()
+    print(f"Available tier 1 subjects --->  {t1}")
+    counter = 2
+    for i in range(number_of_levels,1,-1):
+        print(f"Recomended tier {counter} subject --->  {other_tiers[i-2]}")
+        counter += 1
         
 
 main()
---
+++ test.py
@@ -0,0 +1,23 @@
+import csv
+from collections import Counter
+
+# Read the rows of the CSV file into a list of dictionaries
+with open("ecan_data-17-sep-19.csv") as csvfile:
+  reader = csv.DictReader(csvfile)
+  
+  rows = list(reader)
+
+# Count the number of unique values in each column
+counters = {column: Counter(row[column] for row in rows) for column in reader.fieldnames}
+
+# Sort the columns by the number of unique values, from most to least
+sorted_columns = sorted(counters, key=lambda x: len(counters[x]), reverse=True)
+
+
+# Print the hierarchy
+print("Hierarchy:")
+print(counters[sorted_columns[0]])
+for column in sorted_columns:
+    
+  
+    print(f" - {column}: {len(counters[column])} unique values")
