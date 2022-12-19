ddd5dcb190cd86a43324bf4baf4c19c8bd4b8fb2
Hugh Avery <hav13@uclive.ac.nz>
2022-12-19 16:33:00 +1300

    colourful features

--- save.py
+++ save.py
@@ -1,23 +1,8 @@
 import csv
-from colorama import Fore, Style
+
+CURRENT_HEIRARCHY = 5
 
 
-def good_tier_choice(column_data,col_name):
-    """checks if a column is relevant for model"""
-    ratio = len(column_data) / len(set(column_data))
-
-    #To close to being a tier 1 or all values in column are same
-    if 1 < ratio < 1.2 or len(set(column_data)) == 1:
-        return False
-
-    # allow numbers to be tier choices if used as a unique indicator or is a date within ther range 2000-2030
-    if column_data[0].isnumeric() and column_data[0] not in range(2000,2030):
-        tier1_indicator = ["unique","id","distinct","identifier"]
-        for j in tier1_indicator:
-            if j in col_name:
-                return True
-        return False
-    return True
 
 def read_file_and_organise_hierarchy(filename):
     with open(filename, encoding='utf-8-sig') as file:
@@ -43,10 +28,11 @@ def read_file_and_organise_hierarchy(filename):
 
     for i in dic:
         column_data = dic[i]
-        
+   
         #checks if tier is a good choice and is not numeric and is not a date
         if good_tier_choice(column_data,col_names[i].lower()):
             #tier1
+            print(col_names[i].lower())
             if len(column_data) == len(set(column_data)):
                 candidates_tier1.append(col_names[i])
                 continue
@@ -58,33 +44,55 @@ def read_file_and_organise_hierarchy(filename):
 def main():
     # Files to TEST algorithm 
     # filename = "covid-19 16-April.csv"
-    filename = "covid-19 20-April.csv"
+    # filename = "covid-19 20-April.csv"
     # filename = "ecan_data-17-sep-19.csv"
     # filename = "transport.csv"
-    # filename = "Christchurch NZ data.csv"
-    # filename= "Gisborne Dc LTPB.csv"
+    #filename = "Christchurch NZ data.csv"
+    filename= "Gisborne Dc LTPB.csv"
 
     t1,other_tiers = read_file_and_organise_hierarchy(filename)
     if len(t1) == 0:
         print("No unique columns in csv try with another set of data")
         return None
     Max_number_of_levels = len(other_tiers) + 1
-    number_of_levels = int(input(f"How many levels you would like in your application(Max num layers --> {Max_number_of_levels}) "))
+    number_of_levels = int(input(f"Please specify how many levels you would like in your application(Max num layers --> {Max_number_of_levels}) "))
     while number_of_levels < 1 or number_of_levels > Max_number_of_levels:
         print("Your chosen hierarchy is not valid\ntry again")
-        number_of_levels = int(input(f"How many levels you would like in your application(Max num layers --> {Max_number_of_levels}) "))
-
+        number_of_levels = int(input(f"Please specify how many levels you would like in your application(Max num layers --> {Max_number_of_levels}) "))
 
     
-    print(Fore.RED + f"Available tier 1 subjects --->  {t1}")
+    
+    print(f"Available tier 1 subjects --->  {t1}")
     counter = 2
     other_tiers.reverse()
     for i in range(number_of_levels,1,-1):
-        print(Fore.BLUE + f"Recomended tier {counter} subject --->  {other_tiers[i-2]}")
+        print(f"Recomended tier {counter} subject --->  {other_tiers[i-2]}")
         counter += 1
         
 
-    print(Style.RESET_ALL)
+
+def good_tier_choice(column_data,col_name):
+    """checks if a column is relevant for model"""
+    from save import CURRENT_HEIRARCHY
+    ratio = len(column_data) / len(set(column_data))
+
+    #To close to being a tier 1
+    if 1 < ratio < 1.2:
+        return False
+
+    if column_data[0].isnumeric() and column_data[0] not in range(2000,2030):
+        tier1_indicator = ["unique","id","distinct","identifier"]
+        for j in tier1_indicator:
+            if j in col_name:
+                CURRENT_HEIRARCHY += 5
+                return True
+        return False
+    
+    #all values in column are same 
+    if len(set(column_data)) == 1 or len(set(column_data)) > CURRENT_HEIRARCHY:
+        return False
+    CURRENT_HEIRARCHY += 5
+    return True
 main()
 
 
--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 import csv
 from collections import Counter
 
+
+#Can be used to count number of unique values in a column
+
+
 # Read the rows of the CSV file into a list of dictionaries
-with open("covid-19 20-April.csv") as csvfile:
+# Files to TEST algorithm 
+# filename = "covid-19 16-April.csv"
+# filename = "covid-19 20-April.csv"
+filename = "ecan_data-17-sep-19.csv"
+# filename = "transport.csv"
+#filename = "Christchurch NZ data.csv"
+# filename= "Gisborne Dc LTPB.csv"
+with open(filename) as csvfile:
   reader = csv.DictReader(csvfile)
   
   rows = list(reader)
@@ -16,7 +27,7 @@ sorted_columns = sorted(counters, key=lambda x: len(counters[x]), reverse=True)
 
 # Print the hierarchy
 print("Hierarchy:")
-
+print(counters)
 for column in sorted_columns:
     
   
